a8d697d2ef478363f24d0239547ba502ea12684c
yuanfangchi <yuanfangchi@dhcp-128-189-250-3.ubcsecure.wireless.ubc.ca>
2014-03-20 14:24:57 -0700

    test 15 nodes

--- KVStore/src/KVStore/KVStore.java
+++ KVStore/src/KVStore/KVStore.java
@@ -14,12 +14,24 @@ public class KVStore {
 		String localHostName = InetAddress.getLocalHost().getHostName();
 		Collection<String> nodes = new ArrayList<String>();
 
-		// nodes.add("planetlab2.cs.ubc.ca");
-		// nodes.add("planetlab1.cs.ubc.ca");
+		nodes.add("planetlab2.cs.ubc.ca");
+		nodes.add("planetlab1.cs.ubc.ca");
 		nodes.add("pl-node-1.csl.sri.com");
-		// nodes.add("planetlab-4.eecs.cwru.edu");
+		nodes.add("planetlab-4.eecs.cwru.edu");
+		nodes.add("planetlab-2.cs.auckland.ac.nz");
+		nodes.add("planetlab-2.sysu.edu.cn");
+		nodes.add("planetlab1.acis.ufl.edu");
+		nodes.add("csplanetlab3.kaist.ac.kr");
+		nodes.add("planetlab4.wail.wisc.edu");
+		nodes.add("pl2.eecs.utk.edu");
+		nodes.add("ricepl-5.cs.rice.edu");
+		nodes.add("75-130-96-12.static.oxfr.ma.charter.com");
+		nodes.add("planet-lab4.uba.ar");
+		nodes.add("planetlab2.acis.ufl.edu");
+		nodes.add("planetlab1.cs.uml.edu");
+		
 
-		nodes.add(localHostName);
+		// nodes.add(localHostName);
 		System.out.println(localHostName);
 
 		/*
